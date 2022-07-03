17973518757e3e02935a51a9d7a8f13691a8ab8a
katieb786 <katiebond33091@gmail.com>
2022-07-03 12:16:14 -0400

    if/else statements

---
+++ FirstKotlinProgram/app/src/main/java/com/bond/firstkotlinprogram/WhenStatement.kt
@@ -0,0 +1,24 @@
+package com.bond.firstkotlinprogram
+
+fun main(args: Array<String>) {
+
+    print("Enter enter a day number of the week : ")
+
+    var dayNumber:Int = readLine()!!.toInt()
+    var day:String
+
+    when(dayNumber)
+    {
+        1 -> day = "Monday"
+        2 -> day = "Tuesday"
+        3 -> day = "Wednesday"
+        4 -> day = "Thursday"
+        5 -> day = "Friday"
+        6 -> day = "Saturday"
+        7 -> day = "Sunday"
+        else -> day = "Invalid day choice"
+    }
+
+    println("The day of the week is $day.")
+
+}
